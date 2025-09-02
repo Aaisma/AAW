e720fb9235fb526fb128cca7256352c89f92887b
Aaisma <wagleaaisma07@gmail.com>
2025-09-02 10:52:24 +0545

    Types of Variable

---
+++ typeOfVariable.java
@@ -0,0 +1,11 @@
+public class typeOfVariable {
+
+    void disolay(){
+        int name;
+        int age;
+        System.out.println("name");
+    }
+    public static void main(String[] args) {
+        
+    }
+}
